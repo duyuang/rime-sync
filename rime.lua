44f2e2d9f8d5871e89eff5008f2ef2e033e762d4
duyuang <du-yuang@outlook.com>
2026-02-11 01:09:24 +0800

    first commit

---
+++ rime.lua
@@ -0,0 +1,6 @@
+-- easy_en_enhance_filter: 连续输入增强
+-- 详见 `lua/easy_en.lua`
+local easy_en = require("easy_en")
+easy_en_enhance_filter = easy_en.enhance_filter
+unicode_translator = require("unicode_translator")
+time_translator = require("time")
